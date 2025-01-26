eca67e80207c0f6d77650658494c07ff3184370e
Nithishagoud28 <nithishagoud45@gmail.com>
2025-01-26 15:16:21 +0530

    new functionality

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-let display=document.getElementById("display")
+
 function calc(event){
-     console.log(event.target.value)
+    //  console.log(event.target.value)
+    let display=document.getElementById("display")
+    display.value+=event.target.value
+
 }
+function my_evaluate(){
+    display.value=eval(display.value)
+}
+function del(){
+    display.value=display.value.slice(0,-1)
+}
+function allclear(){
+    display.value=""
+}
+function calculator(){
+    let display=document.getElementById("display")
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="Error";
+    }
+}
+
+    
